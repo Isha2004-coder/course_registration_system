747db8a5f44996112a4fa1f700ccd7bbffafbe45
Isha2004-coder <aroraishanshika@gmail.com>
2025-08-21 12:23:38 -0700

    Update Student.java

--- src/main/java/edu/mini/domain/Student.java
+++ src/main/java/edu/mini/domain/Student.java
@@ -1,7 +1,7 @@
 package edu.mini.domain;
 
 public class Student extends Person {
-  private final boolean safetyTrained; // demonstrates encapsulation
+  private final boolean safetyTrained; // Safety Trained- students require training before enrolling in a Lab class.
 
   public Student(int id, String name, boolean safetyTrained) {
     super(id, name);
